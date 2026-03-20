2c3c1a6ee3b6fefddb0185e77a38003429e74ecd
venkatesh-developer04 <122863624+venkatesh-developer04@users.noreply.github.com>
2026-03-20 18:21:05 +0530

    Update vite.config.js

--- control-panel/vite.config.js
+++ control-panel/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'https://waitros-backend.onrender.com'
+      '/api': 'https://waitro.onrender.com'
     }
   }
 })
